4c5bab9887c3fa8c42708c67e5cbb7bb830c6841
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-04-24 21:38:42 -0300

    Padronizando texto

--- projeto/sugeridorTarefas/SugeridorTarefas.java
+++ projeto/sugeridorTarefas/SugeridorTarefas.java
@@ -5,7 +5,7 @@
     2. Converter para hora totais
     3. Selecionar atividades recomendadas
 
-    -- Faixas de tempo pre definidas
+    -- Faixas de tempo pré-definidas
     - 5 minutos: beber água, levantar e alongar, respirar fundo.
     - 10 minutos: ler um artigo curto, revisar anotações, meditar.
     - 20 minutos: assistir a uma videoaula, fazer uma caminhada curta.
@@ -14,20 +14,41 @@
  */
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SugeridorTarefas{
 
 
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int time, timeMinutos;
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Tempo disponível: ");
+      String time = scanner.nextLine().toLowerCase();
 
+      Pattern horasPattern = Pattern.compile("(\\d+)\\s*(h|hora|horas)");
+      Pattern minutosPattern = Pattern.compile("(\\d+)//s*(m|min|minuto|minutos)");
 
-        System.out.println("Quanto tempo disponível você tem?");
-        time = input.nextInt();
+      Matcher horasMatcher = horasPattern.matcher(time);
+      Matcher minutosMatcher = minutosPattern.matcher(time);
 
-       timeMinutos = time * 60;
+      int totalMinutos = 0;
+      if(horasMatcher.find()){
+          int horas = Integer.parseInt(horasMatcher.group(1));
+          totalMinutos += horas *60;
+      }
+      if (minutosMatcher.find()){
+          int minutos = Integer.parseInt(minutosMatcher.group(1));
+          totalMinutos  += minutos;
+      }
+      if (totalMinutos==0){
+          try {
+              totalMinutos = Integer.parseInt(time.replaceAll("[^0-9]", ""));
+          } catch (NumberFormatException e){
+              System.out.println("Entrada inválida.");
+              return;
+          }
+      }
 
-       System.out.println("Tempo em " + timeMinutos + " minutos");
-
+      System.out.println("Você informou: " + totalMinutos + "minutos disponíveis");
     }
 }
