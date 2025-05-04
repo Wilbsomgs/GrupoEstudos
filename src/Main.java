db50a1ef97db6bba326c8c5221ab05af7924b9ee
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-05-04 17:30:26 -0300

    Documentando código

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+/*
+    1. O sistema gera um **número aleatório entre 1 e 100**.
+    2. O usuário deve adivinhar se o **próximo número será maior ou menor**.
+    3. O sistema gera um novo número e compara:
+        - Se acertou → +1 ponto
+        - Se errou → 0 ponto
+    4. O número novo vira o "atual", e o ciclo se repete.
+    5. O jogo continua até o usuário digitar `"sair"`.
+    6. Ao final, o sistema exibe a **pontuação total**.
+ */
+
 import java.util.Scanner;
 import java.util.Random;
 
