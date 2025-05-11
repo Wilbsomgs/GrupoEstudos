4e1396e2d46446186a51b9c0fb9050998ffbb00c
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-05-11 18:31:40 -0300

    Código finalizado - Decompondo classes em Java

--- semana2/cacaNumeroAtz/CacaNumero2.java
+++ semana2/cacaNumeroAtz/CacaNumero2.java
@@ -1,26 +1,32 @@
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
-public class CacaAoNumero2 {
+public class CacaNumero2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        OutrasFuncionalidades numeroMaiorMenor = new OutrasFuncionalidades();
-        int secretNumber, userNumber, pontuacao, opcao;
+        Random random = new Random();
+
+        VerificacaoResposta verificacao = new VerificacaoResposta();
+        NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+        EncerrarPrograma encerrarPrograma = new EncerrarPrograma();
+        Menu menu = new Menu();
+
+        menu.ExibirMenu();
+
+
+        int secretNumber, userNumber, points, option;
 
         int[] palpites = new int[10];
+
         String opcaoJogarNovamente;
 
-        // Menu
-        System.out.println("-------- Caça Ao número --------");
-        System.out.println("Selecione uma opção: ");
-        System.out.println("1- Jogar ");
-        System.out.println("2- Sair ");
 
-        opcao = scanner.nextInt();
 
-        switch (opcao){
+
+        option = scanner.nextInt();
+
+        switch (option){
             case 1:
                 System.out.println("Iniciando jogo");
                 break;
@@ -33,15 +39,15 @@ public class CacaAoNumero2 {
                 break;
         }
 
-        System.out.println("\nVocê X Máquina\n");
-        System.out.println("O computador irá gerar um número aleatório e você deverá adivinhar qual número foi gerado.");
-        System.out.println("Topa o desafio? Bem o jogo começou!\n");
+        System.out.println("\nVocê VS Máquina\n");
+        System.out.println("O computador irá gerar um número aleatório e você deverá adivinhar qual número foi gerado.\n");
 
-        secretNumber = numeroMaiorMenor.numeroAleatorio();
-        System.out.println("numeroAleatorio" + secretNumber);
+
+        secretNumber = numeroAleatorio.GerarNumeroAleatorio();
+
 
         for(int i=1; i< palpites.length; i++){
-            pontuacao = 0;
+            points = 0;
 
             System.out.println("Qual seu palpite? ");
             userNumber = scanner.nextInt();
@@ -49,15 +55,14 @@ public class CacaAoNumero2 {
 
             palpites[i] = userNumber;
 
-            numeroMaiorMenor.MaiorMenor(userNumber,secretNumber);
+            verificacao.Verificador(userNumber,secretNumber);
 
 
             System.out.println("Você deseja continuar jogando? S/N");
             opcaoJogarNovamente = scanner.nextLine();
             opcaoJogarNovamente = opcaoJogarNovamente.toLowerCase();
 
-            numeroMaiorMenor.EncerrarPrograma(opcaoJogarNovamente);
-
+            encerrarPrograma.EncerrarPrograma(opcaoJogarNovamente);
         }
 
         for(int i =0; i< palpites.length; i++){
---
+++ semana2/cacaNumeroAtz/Menu.java
@@ -0,0 +1,8 @@
+public class Menu {
+    public void ExibirMenu(){
+        System.out.println("-------- Caça Ao número --------");
+        System.out.println("Selecione uma opção: ");
+        System.out.println("1- Jogar ");
+        System.out.println("2- Sair ");
+    }
+}
