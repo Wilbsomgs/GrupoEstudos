bfdcd39e5adefc906ee475377888576e66277afd
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-05-14 22:01:53 -0300

    Adicionando Classe RealizarCalculos

--- semana3/Calculadora.java
+++ semana3/Calculadora.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Calculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculos calcular = new Calculos();
+        RealizarCalculos calcular = new RealizarCalculos();
         String[] allowedOperation = {"+", "-", "*", "/"};
 
         System.out.println("== CALCULADORA BÁSICA ==");
---
+++ semana3/Calculos.java
@@ -0,0 +1,18 @@
+public class RealizarCalculos {
+    public double Soma(double numero1, double numero2){
+        return numero1 + numero2;
+    }
+
+    public double Subtrair(double numero1, double numero2){
+        return numero1 - numero2;
+    }
+
+    public double Multiplicar(double numero1, double numero2){
+        return numero1 * numero2;
+    }
+
+    public double Dividir(double numero1, double numero2){
+        return numero1 / numero2;
+    }
+
+}
