96486d6ba594005da19fcd4531047794dd7100e4
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-05-28 21:29:57 -0300

    Refinando programa JogoForca

---
+++ out/production/GrupoEstudos/README.md
@@ -0,0 +1,30 @@
+Objetivo: 
+Desenvolver um programa de terminal que:
+
+1. Escolha uma palavra secreta para ser adivinhada.
+2. Mostre a palavra como uma sequência de espaços vazios (`_ _ _ _`) indicando as letras ocultas.
+3. Permita que o usuário chute **uma letra por vez**.
+4. Atualize o estado do jogo:
+    - Se a letra existir na palavra, revelar todas as posições corretas.
+    - Se não existir, **contar como erro**.
+5. Limitar o número de tentativas erradas (ex: no máximo 6 erros).
+6. Finalizar o jogo quando:
+    - O usuário adivinhar todas as letras corretamente (vitória).
+    - O usuário errar 6 vezes (derrota).
+
+
+## 🛠️ Especificações Técnicas
+
+- Tecnologia usada: Java
+- A palavra secreta deverá ser sorteada de uma lista simples de palavras.
+- O programa deve rodar inteiramente pelo terminal.
+- Mostrar sempre o estado atual da palavra após cada tentativa.
+- Mostrar o número de tentativas restantes.
+
+  Instruções:
+  - Definir categorias
+  - Definir conjunto de palavras
+  - Criar maneira de sortear valores do array
+  - Solicitar ao usuario a categoria
+  - Preciso converter a qntd de caracteres de uma palavra pra traços
+  - 
--- semana4/JogoForca.java
+++ semana4/JogoForca.java
@@ -1,57 +1,96 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class JogoForca {
-    public static void main(String[] args) {
-        SelecionarCategoria selecionarCategoria = new SelecionarCategoria();
+
+    public static String menu(){
         Scanner scanner = new Scanner(System.in);
 
+        Random random = new Random();
+        String[] animais = new String[]{"Gato", "Peixe", "Ganso", "Formiga", "Cachorro"};
+        String[] frutas = new String[]{"Morango", "Uva", "Banana", "Laranja", "Manga"};
+        String[] paises = new String[]{"Brasil", "Portugal", "Argentina", "Venezuela", "Colombia"};
+
+        System.out.println("----------------------------------------------------------");
         System.out.println("Olá, seja bem vindos(as) ao Jogo da forca: ");
-        String palavraSorteada = selecionarCategoria.selecionarCategoria();
+        System.out.println("Selecione uma categoria: [1]Animais [2]Frutas [3]Paises");
+        System.out.println("-----------------------------------------------------------");
 
-        String palavraMascarada = selecionarCategoria.converterCharacteres(palavraSorteada);
+        int opcao = scanner.nextInt();
 
+        String palavraSorteada = null;
 
-        int tentativas = 6;
+        switch (opcao){
+            case 1:
+                System.out.println("----- ANIMAIS ------");
+                palavraSorteada = sortear(animais);
+                break;
+            case 2:
+                System.out.println("------ FRUTAS -------");
+                palavraSorteada = sortear(frutas);
+                break;
+            case 3:
+                System.out.println("------ PAISES ---------");
+                palavraSorteada = sortear(paises);
+                break;
+            default:
+                System.out.println("Opção inválida, reinicie o programa.");
+        }
+        return palavraSorteada;
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String randomWord = menu();
+
+        String censoredWord = converterCharacteres(randomWord);
+
+        int attempts = 6;
 
         do {
             System.out.println("Digite uma letra: ");
             char chute = scanner.next().charAt(0);
 
-            if(palavraSorteada.contains(String.valueOf(chute))){
+            if(randomWord.contains(String.valueOf(chute))){
                 System.out.println("Letra correta!");
-                palavraMascarada = verificarCaracteres(palavraSorteada, palavraMascarada, chute);
+                censoredWord = verificarCaracteres(randomWord, censoredWord, chute);
 
             } else {
                 System.out.println("Letra incorreta");
 
-                System.out.println(palavraMascarada);
-                System.out.println("Tentativas restantes: " + tentativas);
-                tentativas--;
+                System.out.println(censoredWord);
+                System.out.println("Tentativas restantes: " + attempts);
+                attempts--;
             }
-        } while(tentativas!=0);
+        } while(attempts !=0);
 
     }
 
     public static String verificarCaracteres(String palavraSorteada, String palavraMascarada, char chute){
         char[] arrayPalavraSortaeada = palavraSorteada.toCharArray();
-        char[] arrayPalavraMascarada = palavraMascarada.replace(" ", "").toCharArray();
 
-        for (int i = 0; i < arrayPalavraSortaeada.length; i++) {
-            if (arrayPalavraSortaeada[i] == chute) {
-                arrayPalavraMascarada[i] = chute;
-            }
-        }
-
-        StringBuilder novaOculta = new StringBuilder();
-        for (char c : arrayPalavraMascarada) {
-              novaOculta.append(c).append(" ");
-        }
-
-
-        String resultado = novaOculta.toString().trim();
+        String resultado = "".trim();
         System.out.println("Resultado: " + resultado );
         return resultado;
     }
+
+    public static String sortear(String[] palavra){
+        Random random = new Random();
+
+        int indice = random.nextInt(palavra.length);
+
+        return palavra[indice];
+    }
+
+    public static String converterCharacteres(String palavra){
+        int qntCaracteres = palavra.length();
+
+        String censoredWord = "_".repeat(qntCaracteres);
+        System.out.println( censoredWord + qntCaracteres + " " + " letras");
+
+        return censoredWord;
+    }
 }
+
+
