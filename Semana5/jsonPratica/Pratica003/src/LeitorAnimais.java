501aa00fa612d2389fc39b7d28ba8934f8bb809f
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-06-11 21:09:24 -0300

    Renomeando pastas principais da semana 5 e excluindo antiga

--- Semana5/jsonPratica/Pratica003/src/LeitorAnimais.java
+++ Semana5/jsonPratica/Pratica003/src/LeitorAnimais.java
@@ -3,9 +3,7 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Objects;
+
 public class LeitorAnimais {
    public static void main(String[] args){
        try {
